6195b319c279f3d8d0207d9715ee66151ab49e8b
github username <randyherasme6@gmail.com>
2020-01-24 14:36:08 -0500

    nav bar included

--- src/App.js
+++ src/App.js
@@ -1,33 +1,29 @@
 import React from "react";
 import "./App.css";
+import NavBar from "./NavBar";
 import github from "./github-logo.svg";
 import linkedin from "./linkedin-logo.png";
 
 function App() {
   return (
-    <div className="main">
-      <h1>Randy Herasme</h1>
-      <h2>Software Engineer</h2>
-      <h2>New York, NY</h2>
-      <div className="icon-container">
-        <hr></hr>
-        <a
-          href="https://github.com/graciemcguire"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img className="icon" src={github} alt="github" />
-        </a>
+    <div className="app">
+      <NavBar />
+      <div className="main">
+        <h1>Randy Herasme</h1>
+        <h2>Software Engineer</h2>
+        <h2>New York, NY</h2>
+        <div className="icon-container">
+          <hr></hr>
+          <a href="https://github.com/randyher">
+            <img className="icon" src={github} alt="github" />
+          </a>
 
-        <a
-          href="https://www.linkedin.com/in/gracie-mcguire-43a9b361/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img className="icon" src={linkedin} alt="linkedin" />
-        </a>
+          <a href="https://www.linkedin.com/in/randy-herasme-89822a9a/">
+            <img className="icon" src={linkedin} alt="linkedin" />
+          </a>
+        </div>
+        <div className="icon-links"></div>
       </div>
-      <div className="icon-links"></div>
     </div>
   );
 }
